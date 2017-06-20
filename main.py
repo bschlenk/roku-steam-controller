1d4836d751132e567060ec7a508ed6014631fb5a
Brian Schlenker <bschlenk@umich.edu>
2017-06-19 20:23:57 -0700

    Add command parsing

--- main.py
+++ main.py
@@ -3,13 +3,30 @@
 from settings import ROKU_IP
 from roku import Roku
 import sys
+import argparse
 
 roku = Roku(ROKU_IP)
 
-if len(sys.argv) != 2:
-    print('usage: {} <command>'.format(sys.argv[0]))
-    sys.exit(1)
+def handle_args():
+    parser = argparse.ArgumentParser(description='Press the given button on the roku remote.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('command', nargs='?', help='the command to run')
+    group.add_argument('-c', '--commands', action='store_true', help='list the available commands')
+    parser.add_argument('-r', '--repeat', metavar='n', type=int, default=1, help='repeat the command n times')
+    return parser.parse_args()
 
-command = sys.argv[1]
-getattr(roku, command)()
 
+def main():
+    args = handle_args()
+    if args.commands:
+        print(*roku.commands, sep='\n')
+        return
+
+    command = getattr(roku, args.command)
+    repeat = args.repeat
+
+    for c in range(repeat):
+        command()
+
+
+main()
